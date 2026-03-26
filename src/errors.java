efb8841511b7233bf0cfccea4f9ee61b406a19ab
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-03-26 15:20:58 +0000

    add: error stuff

---
+++ src/errors.java
@@ -0,0 +1,15 @@
+void main() {
+/*
+Checked errors
+FileNotFound, IOException
+unchecked errors
+NullPointer, Arithmetic
+*/
+
+    //Risk code goes in try catch
+    try {
+        IO.println("Welcome to Java Programming System");
+    } catch (Exception e) {
+        IO.println("Please enter a valid choice");
+    }
+}
